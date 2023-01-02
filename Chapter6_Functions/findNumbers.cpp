e8b5f604bcbcb64eda4fb2d277d7adba9c3ce797
TedXpro <johannes.woldeyes@gmail.com>
2023-01-02 06:04:23 +0300

    Accept numbers in array form

--- Chapter6_Functions/findNumbers.cpp
+++ Chapter6_Functions/findNumbers.cpp
@@ -4,45 +4,53 @@ and give the number the user wants.*/
 
 using namespace std;
 
+void acceptNumbers(int[], int);
 bool checkNumbers(int, int, int);
 
 int main()
 {
-    int first, last;
-    cout << "Enter the first number: ";
-    cin >> first;
-    cout << "Enter the last number: ";
-    cin >> last;
+    int size;
+    cout << "Enter the size of the array: ";
+    cin >> size;
 
-    int number;
+    int numbers[size];
+    acceptNumbers(numbers, size);
+
+    int n;
     cout << "Enter the number: ";
-    cin >> number;
+    cin >> n;
 
-    int counter = 0;
-
-    for (int i = first; i <= last; i++)
+    for (int i = 0; i < (size - 1); i++)
     {
-        for (int j = first; j <= last; j++)
+        for (int j = i + 1; j < size; j++)
         {
-            if (checkNumbers(i, j, number))
-            {
-                counter++;
-                cout << i << " & " << j << endl;
-                if (i == (number / 2))
-                    exit(0);
-            }
+            if (checkNumbers(numbers[i], numbers[j], n))
+                cout << numbers[i] << " " << numbers[j] << endl;
         }
     }
-    if (counter == 0)
-        cout << "I cant find any number between " << first << " and " << last
-             << " that can add up to give " << number << endl;
+}
+
+/**
+ * Accept Numbers.
+ *
+ * @param arrSize is the size of the array.
+ * @param nums is the array that will contain the numbers.
+ */
+void acceptNumbers(int nums[], int arrSize)
+{
+    for (int i = 0; i < arrSize; i++)
+    {
+        cout << "Enter element #" << (i + 1) << ": ";
+        cin >> nums[i];
+    }
 }
 
 /**
  * Check if the first two parameters add up to give the third parameter.
  *
- * @param the first two contain the numbers to be summed, the last
- * contains the number to be checked.
+ * @param f is the firstNumber to be summed.
+ * @param l is the secondNumber to be summed.
+ * @param num is the number to checked to.
  * @return 1 if the numbers add up or 0 if the numbers dont.
  */
 bool checkNumbers(int f, int l, int num)
