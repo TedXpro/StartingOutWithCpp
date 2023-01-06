caa3e10a4ec10e19d89ef2e9873e97ba31d4ae48
TedXpro <johannes.woldeyes@gmail.com>
2023-01-06 14:15:07 +0300

    Make some adjustment by adding function prototype

--- Chapter7_Arrays/TicTacToeGame/ticTacToe.cpp
+++ Chapter7_Arrays/TicTacToeGame/ticTacToe.cpp
@@ -5,141 +5,14 @@ This program is game of tic tac toe.*/
 
 using namespace std;
 
-/**
- * This function displays the positions of the objects.
- */
-void display(char matrix[3][3])
-{
-    for (int i = 0; i < 3; i++)
-    {
-        for (int j = 0; j < 3; j++)
-        {
-            cout << setw(4) << matrix[i][j] << " ";
-        }
-        cout << endl;
-    }
-}
-
-/**
- * This function accpets the row and column from a player.
- */
-void acceptPosition(int numbers[])
-{
-    int row, col;
-    cout << "Enter the row of the matrix: ";
-    cin >> row;
-    cout << "Enter the column of the matrix: ";
-    cin >> col;
-
-    numbers[0] = row;
-    numbers[1] = col;
-}
-
-/**
- * This function test the validity of the row and column that are entered by the player.
- */
-bool testValidity(int numbers[])
-{
-    int row = numbers[0];
-    int col = numbers[1];
-
-    return ((row >= 0 && row < 3) && (col >= 0 && col < 3));
-}
-
-/**
- * This function test whether the row and column entered by the player was taken or not.
- */
-bool isNotTaken(char objects[3][3], int positions[])
-{
-    return (objects[positions[0]][positions[1]] == '*');
-}
-
-/**
- * This function tests the positions of 'X' or 'O' diagonally on every round.
- */
-bool testDiagonal(char objects[3][3])
-{
-    int fdCounter1 = 0, fdCounter2 = 0;    // forward counter
-    int bkCounter1 = 0, bkCounter2 = 0;    // backward counter
-    for (int i = 0; i < 3; i++)
-    {
-        for (int j = 0; j < 3; j++)
-        {
-            if (i == j)
-            {
-                if (objects[i][j] == 'X')
-                    fdCounter1++;
-                if (objects[i][j] == 'O')
-                    fdCounter2++;
-            }
-            if ((i + j) == 2)
-            {
-                if (objects[i][j] == 'X')
-                    bkCounter1++;
-                if (objects[i][j] == 'O')
-                    bkCounter2++;
-            }
-        }
-        if ((fdCounter1 == 3) || (fdCounter2 == 3))
-            return true;
-        else if ((bkCounter1 == 3) || (bkCounter2 == 3))
-            return true;
-    }
-
-    return false;
-}
-
-/**
- * This function test the position of 'X' or 'O' in a row on every round.
-*/
-bool testRow(char objects[3][3])
-{
-    for (int i = 0; i < 3; i++)
-    {
-        int counter1 = 0;
-        int counter2 = 0;
-        for (int j = 0; j < 3; j++)
-        {
-            if (objects[i][j] == 'X')
-                counter1++;
-            if (objects[i][j] == 'O')
-                counter2++;
-        }
-        if ((counter1 == 3) || (counter2 == 3))
-            return true;
-    }
-    return false;
-}
-
-/**
- * This function test the position of 'X' or 'O' in a column on every round.
- */
-bool testColumn(char objects[3][3])
-{
-    for (int i = 0; i < 3; i++)
-    {
-        int counter1 = 0;
-        int counter2 = 0;
-        for (int j = 0; j < 3; j++)
-        {
-            if (objects[j][i] == 'X')
-                counter1++;
-            if (objects[j][i] == 'O')
-                counter2++;
-        }
-        if ((counter1 == 3) || (counter2 == 3))
-            return true;
-    }
-    return false;
-}
-
-/**
- * This function calls the three functions above and tries to find a winner.
-*/
-bool testWinner(char objects[3][3])
-{
-    return (testColumn(objects) || testDiagonal(objects) || testRow(objects));
-}
+void acceptPosition(int[]);
+bool testValidity(int[]);
+bool isNotTaken(char[][3], int[]);
+bool testDiagonal(char[][3]);
+bool testRow(char[][3]);
+bool testColumn(char[][3]);
+bool testWinner(char [][3]);
+void display(char[][3]);
 
 int main()
 {
@@ -212,7 +85,6 @@ int main()
                 cout << "This game is a draw\n";
                 flag = false;
             }
-            
         }
         cout << "Do u wish to continue (Y/N): ";
         cin >> ch;
@@ -220,4 +92,170 @@ int main()
     cout << "Thank you for playing!!" << endl;
 
     return 0;
+}
+
+/**
+ * This function displays the positions of the objects.
+ *
+ * @param matrix holds the matrix.
+ */
+void display(char matrix[3][3])
+{
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            cout << setw(4) << matrix[i][j] << " ";
+        }
+        cout << endl;
+    }
+}
+
+/**
+ * This function accpets the row and column from a player.
+ *
+ * @param numbers will hold the position of the matrix.
+ */
+void acceptPosition(int numbers[])
+{
+    int row, col;
+    cout << "Enter the row of the matrix: ";
+    cin >> row;
+    cout << "Enter the column of the matrix: ";
+    cin >> col;
+
+    numbers[0] = row;
+    numbers[1] = col;
+}
+
+/**
+ * This function test the validity of the row and column that are entered by the player.
+ *
+ * @param numbers holds the position entered by the user.
+ *
+ * @return 1 if the entered position is between 0 and 2.
+ * @return 0 if the entered position is not between 0 and 2.
+ */
+bool testValidity(int numbers[])
+{
+    int row = numbers[0];
+    int col = numbers[1];
+
+    return ((row >= 0 && row < 3) && (col >= 0 && col < 3));
+}
+
+/**
+ * This function test whether the row and column entered by the player was taken or not.
+ *
+ * @param objects holds the objects array.
+ * @param positions holds the position given by the user.
+ *
+ * @return 1 is the position is not taken.
+ * @return 0 is the position is taken.
+ */
+bool isNotTaken(char objects[3][3], int positions[])
+{
+    return (objects[positions[0]][positions[1]] == '*');
+}
+
+/**
+ * This function tests the positions of 'X' or 'O' diagonally on every round.
+ */
+bool testDiagonal(char objects[3][3])
+{
+    int fdCounter1 = 0, fdCounter2 = 0; // forward counter
+    int bkCounter1 = 0, bkCounter2 = 0; // backward counter
+    for (int i = 0; i < 3; i++)
+    {
+        for (int j = 0; j < 3; j++)
+        {
+            if (i == j)
+            {
+                if (objects[i][j] == 'X')
+                    fdCounter1++;
+                if (objects[i][j] == 'O')
+                    fdCounter2++;
+            }
+            if ((i + j) == 2)
+            {
+                if (objects[i][j] == 'X')
+                    bkCounter1++;
+                if (objects[i][j] == 'O')
+                    bkCounter2++;
+            }
+        }
+        if ((fdCounter1 == 3) || (fdCounter2 == 3))
+            return true;
+        else if ((bkCounter1 == 3) || (bkCounter2 == 3))
+            return true;
+    }
+
+    return false;
+}
+
+/**
+ * This function calls the three functions above and tries to find a winner.
+ *
+ * @param objects will hold the objects.
+ *
+ * @return 1 if there is a winner.
+ * @return 0 if there is no winner.
+ */
+bool testWinner(char objects[3][3])
+{
+    return (testColumn(objects) || testDiagonal(objects) || testRow(objects));
+}
+
+/**
+ * This function test the position of 'X' or 'O' in a row on every round.
+ *
+ * @param objects will hold the objects.
+ *
+ * @return 1 if the position is not taken.
+ * @return 0 is the position is taken.
+ */
+bool testRow(char objects[3][3])
+{
+    for (int i = 0; i < 3; i++)
+    {
+        int counter1 = 0;
+        int counter2 = 0;
+        for (int j = 0; j < 3; j++)
+        {
+            if (objects[i][j] == 'X')
+                counter1++;
+            if (objects[i][j] == 'O')
+                counter2++;
+        }
+        if ((counter1 == 3) || (counter2 == 3))
+            return true;
+    }
+    return false;
+}
+
+/**
+ * This function test the position of 'X' or 'O' in a column on every round.
+ *
+ * @param objects will hold the objects array.
+ *
+ * @return 1 if the position is taken
+ * @return 0 if the position is not taken.
+ */
+bool testColumn(char objects[3][3])
+{
+    for (int i = 0; i < 3; i++)
+    {
+        int counter1 = 0;
+        int counter2 = 0;
+        for (int j = 0; j < 3; j++)
+        {
+            if (objects[j][i] == 'X')
+                counter1++;
+            if (objects[j][i] == 'O')
+                counter2++;
+        }
+        if ((counter1 == 3) || (counter2 == 3))
+            return true;
+    }
+    return false;
 }
