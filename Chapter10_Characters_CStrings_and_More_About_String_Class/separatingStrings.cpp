645376288087b0b90dfaee8964daa806ad144433
TedXpro <johannes.woldeyes@gmail.com>
2023-01-18 12:07:14 +0300

    separate numbers given by the number with comma

---
+++ Chapter10_Characters_CStrings_and_More_About_String_Class/separatingStrings.cpp
@@ -0,0 +1,49 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<string> separateString(string);
+
+int main()
+{
+    string str; // will hold the string but are numbers separated by comma.
+    cout << "Enter the string: ";
+    cin >> str;   // 23,4,56
+
+    vector<string> stringList;
+    stringList = separateString(str);
+
+    for (int i = 0; i < stringList.size(); i++)
+        cout << stringList[i] << endl;
+}
+
+/**
+ * This function will separate the strings upto the comma(,).
+ *
+ * @param answer holds the string entered by the user.
+ *
+ * @return a vector that holds separated numbers.
+ */
+vector<string> separateString(string answer)
+{
+    char ch = ',';
+    vector<string> separate;
+    int count = 0;
+    while (count < answer.size() )
+    {
+        // answer = "23,4,56"
+        string ans;
+        while (answer[count] != ch)
+        {
+            ans += answer[count];
+            count++;
+            if (count == answer.size() )
+                break;
+        }
+        // cout << ans << endl;
+        count++;
+        separate.push_back(ans);
+    return separate;
+}
